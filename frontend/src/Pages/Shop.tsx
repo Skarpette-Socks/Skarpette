7c0d633bd961c55e2c1d11b4ee50bd1a7a9a1f68
Dmytro <dmytrokovaliak@gmail.com>
2024-06-28 09:14:43 +0200

    Your favourites and New goods components added

--- frontend/src/Pages/Shop.tsx
+++ frontend/src/Pages/Shop.tsx
@@ -2,12 +2,18 @@ import CategoriesCarousel from "../Components/CategoriesCarousel/CategoriesCarou
 import PromoCards from "../Components/PromoCards/PromoCards";
 // import ReviewsSlider from "../Components/ReviewsSlider/ReviewsSlider";
 import CustomSlider from "../Components/Slider/CustomSlider";
+import MainPageListGoods from "../Components/MainPageListGoods/MainPageListGoods";
 
 const Shop: React.FC = () => {
+  const newGoods = 'Новинки';
+  const yourFavorite = 'Ваші улюбленці';
+
   return (
     <div>
       <CustomSlider />
       <CategoriesCarousel />
+      <MainPageListGoods title={newGoods}/>
+      <MainPageListGoods title={yourFavorite}/>
       <PromoCards />
       {/* <ReviewsSlider /> */}
     </div>
