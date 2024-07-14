d3420c6888f283d0dcc29df8479a00c3f81fa12a
ho4upitsu <ho4upitsu@gmail.com>
2024-07-14 14:41:50 +0300

    added fitlering for images

--- backend/routes/skarpette.js
+++ backend/routes/skarpette.js
@@ -3,7 +3,18 @@ const multer = require('multer');
 const skarpetteController = require('../controllers/skarpette');
 
 const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 3 * 1024 * 1024,
+    },
+    fileFilter: (req, file, cb) => {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Only image files are allowed'), false);
+        }
+        cb(null, true);
+    },
+});
 
 router.delete('/clear', skarpetteController.clearDB);
 router.get('/search', skarpetteController.getSkarpettesByNameOrVendorCode);
