7252457e1cb9c3ae28bd74e2f827a1930f6adf4f
Dmytro <dmytrokovaliak@gmail.com>
2024-08-04 06:58:04 +0200

    deleted double link declaring

--- frontend/src/Components/Dropdown/Dropdown.tsx
+++ frontend/src/Components/Dropdown/Dropdown.tsx
@@ -6,7 +6,6 @@ import arrow_right from "../assets/img/icons/chevron-right.svg";
 import arrow_up from "../assets/img/icons/caret-up-filled.svg";
 import arrow_down from "../assets/img/icons/caret-down-filled.svg";
 import options from "../../../../backend/bd/categories.json";
-import { Link } from "react-router-dom";
 
 const Dropdown: React.FC = () => {
   const [isActive, setIsActive] = useState(false);
