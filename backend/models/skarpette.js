61aa7760f3979c0373b96a0c0852f38017a3e5fd
ho4upitsu <ho4upitsu@gmail.com>
2024-08-07 22:23:30 +0300

    add: random vendor_code fix: sizes and composition_and_care

--- backend/models/skarpette.js
+++ backend/models/skarpette.js
@@ -1,5 +1,19 @@
 const mongoose = require('mongoose');
 
+const sizeSchema = new mongoose.Schema(
+    {
+        size: {
+            type: String,
+            required: true,
+        },
+        is_available: {
+            type: Boolean,
+            default: true,
+        },
+    },
+    { _id: false }
+);
+
 const skarpetteSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -27,12 +41,7 @@ const skarpetteSchema = new mongoose.Schema({
             required: true,
         },
     ],
-    size: [
-        {
-            type: String,
-            required: true,
-        },
-    ],
+    size: [sizeSchema],
     images_urls: [
         {
             type: String,
@@ -40,15 +49,8 @@ const skarpetteSchema = new mongoose.Schema({
         },
     ],
     composition_and_care: {
-        composition: {
-            type: Map,
-            of: Number,
-            required: true,
-        },
-        care: {
-            type: String,
-            required: true,
-        },
+        type: String,
+        required: true,
     },
     type: {
         type: String,
@@ -70,6 +72,23 @@ const skarpetteSchema = new mongoose.Schema({
         type: Boolean,
         default: true,
     },
+});
+
+function getSizesByType(type) {
+    const sizes = {
+        Men: ['25-27', '27-29', '29-31'],
+        Women: ['23-25', '25-27'],
+        Child: ['16', '18', '19-21', '21-23', '23-25'],
+    };
+
+    return sizes[type].map((size) => ({ size, is_available: true }));
+}
+
+skarpetteSchema.pre('save', function (next) {
+    if (this.size.length === 0 && getSizesByType(this.type)) {
+        this.size = getSizesByType(this.type);
+    }
+    next();
 });
 
 const Skarpette = mongoose.model('Skarpette', skarpetteSchema);
