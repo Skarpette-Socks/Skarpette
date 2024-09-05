368e58edafed2bdcd2782efc3727ddff9fdc49a9
Nataliia Miroshnyk <natamrshn@gmail.com>
2024-09-05 12:14:04 +0100

    improve contacts form

--- frontend/src/Components/ContactsForm/ContactsForm.tsx
+++ frontend/src/Components/ContactsForm/ContactsForm.tsx
@@ -1,11 +1,28 @@
+import  { useState } from "react";
 import "./ContactsForm.scss";
 
 const ContactsForm = () => {
+  const [message, setMessage] = useState<string>("");
+  const [error, setError] = useState<boolean>(false);
+
+  const handleMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const inputValue = e.target.value;
+
+    if (inputValue.length <= 500) {
+      setMessage(inputValue);
+      setError(false);
+    } else {
+      setError(true);
+    }
+  };
+
+
   return (
     <form action="#" className="contacts-form">
       <p className="contacts-form__title" id="contacts-form__title">
         Форма для зв’язку
       </p>
+
       <input
         type="text"
         required
@@ -25,14 +42,22 @@ const ContactsForm = () => {
       <textarea
         required
         placeholder="Повідомлення"
-        className="contacts-form__message"
-        maxLength={300}
+        className={`contacts-form__message ${error ? "contacts-form__message-error" : ""}`}
+        value={message}
+        onChange={handleMessageChange}
       ></textarea>
+
+      {error && (
+        <p className="contacts-form__error-message">
+          Помилка: перевищено максимально допустиму кількість символів.
+        </p>
+      )}
 
       <input
         type="submit"
         value="Надіслати"
         className="contacts-form__button"
+        disabled={error}
       />
     </form>
   );
