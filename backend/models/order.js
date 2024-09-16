4d3793dddcd3ec530e58e4b06555083af4120454
ho4upitsu <ho4upitsu@gmail.com>
2024-09-16 16:22:16 +0300

    add recepient lastName

--- backend/models/order.js
+++ backend/models/order.js
@@ -77,40 +77,55 @@ const orderSchema = new mongoose.Schema({
         default: false,
         required: true,
     },
-    recipientData: [
-        {
-            name: {
-                type: String,
-                validate: {
-                    validator: function (v) {
-                        // Перевірка на існування recipientData та наявність першого елемента
-                        return this.recipientData &&
-                            this.recipientData[0] &&
-                            this.recipientData[0].name
-                            ? !!this.recipientData[0].phoneNumber
-                            : true;
-                    },
-                    message:
-                        'If recipient name is provided, phoneNumber must be provided as well.',
+    recipientData: {
+        firstName: {
+            type: String,
+            validate: {
+                validator: function (v) {
+                    // Перевірка на існування recipientData та наявність першого елемента
+                    return this.recipientData &&
+                        this.recipientData[0] &&
+                        this.recipientData[0].name
+                        ? !!this.recipientData[0].phoneNumber
+                        : true;
                 },
-            },
-            phoneNumber: {
-                type: String,
-                validate: {
-                    validator: function (v) {
-                        // Перевірка на існування recipientData та наявність першого елемента
-                        return this.recipientData &&
-                            this.recipientData[0] &&
-                            this.recipientData[0].phoneNumber
-                            ? !!this.recipientData[0].name
-                            : true;
-                    },
-                    message:
-                        'If recipient phoneNumber is provided, name must be provided as well.',
-                },
+                message:
+                    'If recipient name is provided, phoneNumber must be provided as well.',
             },
         },
-    ],
+        lastName: {
+            type: String,
+            validate: {
+                validator: function (v) {
+                    // Перевірка на існування recipientData та наявність першого елемента
+                    return this.recipientData &&
+                        this.recipientData[0] &&
+                        (this.recipientData[0].name ||
+                            this.recipientData[0].phoneNumber)
+                        ? !!this.recipientData[0].lastName
+                        : true;
+                },
+                message:
+                    'If recipient name or phoneNumber is provided, lastName must be provided as well.',
+            },
+        },
+        phoneNumber: {
+            type: String,
+            validate: {
+                validator: function (v) {
+                    // Перевірка на існування recipientData та наявність першого елемента
+                    return this.recipientData &&
+                        this.recipientData[0] &&
+                        this.recipientData[0].phoneNumber
+                        ? !!this.recipientData[0].name
+                        : true;
+                },
+                message:
+                    'If recipient phoneNumber is provided, name must be provided as well.',
+            },
+        },
+    },
+
     isCompleted: {
         type: Boolean,
         default: false,
