6c6138601f3c4488b562291522d74be2f338d58d
ho4upitsu <ho4upitsu@gmail.com>
2024-09-17 14:03:34 +0300

    add totalPrice fix update order

--- backend/models/order.js
+++ backend/models/order.js
@@ -131,7 +131,7 @@ const orderSchema = new mongoose.Schema({
         default: false,
         required: true,
     },
-    total: {
+    totalPrice: {
         type: Number,
         required: true,
     },
