beae05ee02597680b76616875c67a3e0fbefc7b8
Nataliia Miroshnyk <natamrshn@gmail.com>
2024-10-01 10:42:40 +0100

    add markup for empty cart

--- frontend/src/Components/CartContent/CartContent.tsx
+++ frontend/src/Components/CartContent/CartContent.tsx
@@ -1,7 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useState } from "react";
 import { useCartItems } from "../../Context/CartContext";
 import "./CartContent.scss";
 import CartContentItem from "./CartContentItem";
+import { Link } from "react-router-dom";
+
+import no_cart from '../assets/img/NoItemsCart.svg';
 
 const CartContent: React.FC = () => {
   const { cartItems } = useCartItems();
@@ -75,7 +79,9 @@ const CartContent: React.FC = () => {
                     {totalDiscount > 0 && (
                       <div className="cart__disc">
                         <div className="cart__text">Знижка</div>
-                        <div className="cart__greytext">-{totalDiscount} грн</div>
+                        <div className="cart__greytext">
+                          -{totalDiscount} грн
+                        </div>
                       </div>
                     )}
                   </>
@@ -95,14 +101,21 @@ const CartContent: React.FC = () => {
             </div>
           </>
         ) : (
-          <div className="cart__empty">
-            <div className="cart__empty-text">Ваш кошик порожній :(</div> 
-            <a 
-              href="/catalog/all-socks" 
-              className="cart__empty-link"
-            >
-              Перейти до каталогу
-            </a>
+          <div className="cart__no-item">
+            <img
+              src={no_cart}
+              alt="no_fav"
+              className="cart__no-item-img"
+            />
+            <h3 className="cart__no-item-main-text">Кошик порожній</h3>
+            <p className="cart__no-item-paragraph">
+              Кошик нудьгує на самоті... час це змінити :)
+            </p>
+            <Link to="/catalog/all-socks">
+              <button className="cart__no-item-button">
+                Перейти до каталогу
+              </button>
+            </Link>
           </div>
         )}
       </div>
