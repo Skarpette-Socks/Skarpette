e50ffcf9b4f822f117dfa411ac972638ff0106d7
Dmytro <dmytrokovaliak@gmail.com>
2024-10-25 00:23:47 +0200

    delete button realized

---
+++ admin/src/api/deleteItem.ts
@@ -0,0 +1,27 @@
+export const deleteItem = async (id: string) => {
+  const token = localStorage.getItem("authToken");
+
+  if (!token) {
+    throw new Error("Token is missing");
+  }
+
+  try {
+    const response = await fetch(`http://localhost:5000/skarpette/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Fetch Error:", error);
+    throw error;
+  }
+};
