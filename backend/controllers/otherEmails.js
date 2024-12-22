56d3a4e56a47c1205af6d540f63e276ef0621945
ho4upitsu <ho4upitsu@gmail.com>
2024-12-22 10:14:34 +0200

    add new email for feedback and help form

--- backend/controllers/otherEmails.js
+++ backend/controllers/otherEmails.js
@@ -16,19 +16,48 @@ const sendFeedbackToOwner = async (req, res) => {
         },
         secure: true,
     });
+
+    let filledStars = "";
+    for (let i = 0; i < feedback.score; i++) {
+        filledStars += `
+    <span style="font-size: 18px; display: inline-block; line-height: 1; color: #FFC000;">★</span>`;
+    }
+
+    let emptyStars = "";
+    for (let i = 0; i < 5 - feedback.score; i++) {
+        emptyStars += `
+    <span style="font-size: 18px; display: inline-block; line-height: 1;">✩</span>`;
+    }
+
+    const starRating = filledStars + emptyStars;
+
     const mailOptions = {
         from: `"Skarpette" ${process.env.EMAIL_USER}`,
         to: process.env.EMAIL_USER,
         subject: "Новий відгук в магазині Skarpette!",
         html: `
-            <h1>Новий відгук в магазині Skarpette!</h1>
-            <p>Відгук:</p>
-            <ul>
-                <li>Оцінка: ${feedback.score}</li>
-                <li>Ім'я: ${feedback.firstname}</li>
-                <li>Прізвище: ${feedback.lastname}</li>
-                <li>Текст відгуку: ${feedback.comment}</li>
-            <ul>
+              <div style="
+                    font-family: MacPaw Fixel, sans-serif;
+                    max-width: 800px;
+                    margin-inline: auto;
+                ">
+                    <h1 style="font-size: 35px;">
+                        На Вашому сайті Skarpette.com.ua, залишили нове звернення
+                    </h1>
+                    <div>
+                        <h4 style="margin: 0; font-size: 18px;"><b>ПІБ:</b> ${feedback.firstname} ${feedback.lastname}</h4>
+                        <h4 style="margin: 0; font-size: 18px;"><b>Пошта:</b> ${feedback.email}</h4>
+                        <!-- Виправлений контейнер для оцінки -->
+                        <div style="margin-top: 5px;">
+                            <p style="font-size: 18px; margin: 0;"><b>Оцінка:</b> ${starRating}</p>
+                        </div>
+                    </div>
+                    <p style="
+                        color: rgb(60, 60, 60);
+                        margin-top: 25px;
+                        font-size: 18px;
+                    ">${feedback.comment}</p>
+                </div>
         `,
     };
     try {
@@ -57,12 +86,24 @@ const sendHelpFormToOwner = async (req, res) => {
         to: process.env.EMAIL_USER,
         subject: "Нове запитання у магазині Skarpette!",
         html: `
-            <h1>Нове запитання у магазині Skarpette!</h1>
-            <ul>
-                <li>Ім'я: ${helpBody.firstname}</li>
-                <li>Email: ${helpBody.email}</li>
-                <li>Запитання: ${helpBody.comment}</li>
-            <ul>
+            <div style="
+              font-family: MacPaw Fixel, sans-serif;
+              max-width: 800px;
+              margin-inline: auto;
+            ">
+              <h1 style="font-size: 35px;">
+                На Вашому сайті Skarpette.com.ua, залишили нове звернення
+              </h1>
+              <div>
+                <h4 style="margin: 0; font-size: 18px;"><b>ПІБ:</b> ${helpBody.firstname}</h4>
+                <h4 style="margin: 0; font-size: 18px;"><b>Пошта:</b> ${helpBody.email}</h4>
+              </div>
+              <p style="
+                color: rgb(60, 60, 60);
+                margin-top: 25px;
+                font-size: 18px;
+              ">${helpBody.comment}</p>
+            </div>
         `,
     };
     try {
